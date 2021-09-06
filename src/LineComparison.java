3f0eeb2d172441228091d5f7947d7e6995038a9c
aravindkarri <aravindkarri98495@gmail.com>
2021-09-06 23:54:59 +0530

    ADDED: check two lines are equal or not

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,14 +1,22 @@
 public class LineComparison
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        
-        System.out.println("Welcome to Line Comparison Computation");
         int x1=3,x2=5,y1=2,y2=6;
-        double length = (Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
-        System.out.printf("length of line is = %.2f ",length);
+        Double LENGTH_OF_FIRST_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+        int x3=3,x4=5,y3=2,y4=6;
+        Double LENGTH_OF_SECOND_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+        System.out.printf("Length of First Line is :: %.2f \n",LENGTH_OF_FIRST_LINE);
+        System.out.printf("Length of Second Line is :: %.2f \n",LENGTH_OF_SECOND_LINE);
 
+        if (LENGTH_OF_FIRST_LINE.equals(LENGTH_OF_SECOND_LINE) )
+        {
+            System.out.println("Lines Are Equal");
+        }
+        else
+        {
+            System.out.println("Lines are not equal");
+        }
     }
-
 }
 
