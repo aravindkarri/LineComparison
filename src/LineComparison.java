c99421349443154805c6211f5ed7d29daffe1c53
aravindkarri <aravindkarri98495@gmail.com>
2021-09-16 14:48:20 +0530

    Added: line comparison problem using oops concepts

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,14 +1,7 @@
 public class LineComparison
 {
-    public static void main(String[] args)
+    public void equalsMethod(Double LENGTH_OF_FIRST_LINE,Double LENGTH_OF_SECOND_LINE)
     {
-        int x1=3,x2=5,y1=2,y2=6;
-        Double LENGTH_OF_FIRST_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
-        int x3=3,x4=5,y3=2,y4=6;
-        Double LENGTH_OF_SECOND_LINE = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
-        System.out.printf("Length of First Line is :: %.2f \n",LENGTH_OF_FIRST_LINE);
-        System.out.printf("Length of Second Line is :: %.2f \n",LENGTH_OF_SECOND_LINE);
-
         if (LENGTH_OF_FIRST_LINE.equals(LENGTH_OF_SECOND_LINE) )
         {
             System.out.println("Lines Are Equal");
@@ -17,6 +10,9 @@ public class LineComparison
         {
             System.out.println("Lines are not equal");
         }
+    }
+    public void compareToMethod(Double LENGTH_OF_FIRST_LINE,Double LENGTH_OF_SECOND_LINE)
+    {
         int compare = LENGTH_OF_FIRST_LINE.compareTo(LENGTH_OF_SECOND_LINE);
         if ( compare == 0)
         {
@@ -30,6 +26,20 @@ public class LineComparison
         {
             System.out.println("Line 1 is greater than Line 2");
         }
+    }
+    public static double lengthOfLine(int x1,int y1,int x2,int y2)
+    {
+        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+    }
+    public static void main(String[] args)
+    {
+        Double LENGTH_OF_FIRST_LINE = lengthOfLine(2,5,6,10);
+        Double LENGTH_OF_SECOND_LINE = lengthOfLine(4,7,2,9);
+        System.out.printf("Length of First Line is :: %.2f \n",LENGTH_OF_FIRST_LINE);
+        System.out.printf("Length of Second Line is :: %.2f \n",LENGTH_OF_SECOND_LINE);
+        LineComparison obj = new LineComparison();
+        obj.equalsMethod(LENGTH_OF_FIRST_LINE,LENGTH_OF_SECOND_LINE);
+        obj.compareToMethod(LENGTH_OF_FIRST_LINE,LENGTH_OF_SECOND_LINE);
     }
 }
 
